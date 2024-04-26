9520498e2b447da056edf9b5142b20f2e6f505da
Miso Menze <misomenze6@gmail.com>
2024-04-26 17:04:36 +0200

    initiate google calender api

--- __main__.py
+++
@@ -1,4 +0,0 @@
-from src.services.fixture_processor import FixtureProccessor
-
-fp = FixtureProccessor()
-games = fp.procces_fixtures()
---
+++ main.py
@@ -0,0 +1,11 @@
+from src.services.fixture_processor import FixtureProccessor
+
+def main():
+    fp = FixtureProccessor()
+    games = fp.procces_fixtures()
+
+
+main()
+
+# if __name__ == main:
+#     main()
